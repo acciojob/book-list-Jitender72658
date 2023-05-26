82e9c679b22021d37a02e778b5b20b0f934d5099
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-05-26 16:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const form = document.getElementById('form-group');
+const tableBody = document.querySelector('table table-striped');
+
+// Event listener for form submission
+form.addEventListener('submit', function(e) {
+  e.preventDefault(); // Prevent form submission
+
+  // Get form input values
+  const name = document.getElementById('title').value;
+  const email = document.getElementById('author').value;
+	const isbn = document.getElementById('isbn').value;
+
+  // Create new table row
+  const newRow = document.createElement('tr');
+
+  // Create table data cells and set their content
+  const titleCell = document.createElement('td');
+  titleCell.textContent = title;
+
+  const authorCell = document.createElement('td');
+  authorCell.textContent = author;
+
+ const isbnCell = document.createElement('td');
+  isbnCell.textContent = isbn;
+
+  // Append cells to the new row
+  newRow.appendChild(titleCell);
+  newRow.appendChild(authorCell);
+  newRow.appendChild(isbnCell);
+
+  // Append the new row to the table body
+  tableBody.appendChild(newRow);
+
+  // Clear form inputs
+  form.reset();
+});
